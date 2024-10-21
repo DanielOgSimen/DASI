ba51857926569f72f42b7a38e2599009f03b358d
Daniel Johan Sørby <81819141+loliskrem@users.noreply.github.com>
2024-10-21 09:07:10 +0200

    Endret på hjemme siden til å kun vise knappen, og fikk cta animasjonsknappen til å funke

--- Dasi/src/routes/+page.svelte
+++ Dasi/src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import CtaButton from '../components/cta-button.svelte';
+</script>
+
+<CtaButton text = "Prøv Oss"/>
