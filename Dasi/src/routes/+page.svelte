dc9e508a5f07aee5d34be5a5cc01d4e0c40f2e80
Daniel Johan Sørby <81819141+loliskrem@users.noreply.github.com>
2024-10-29 11:14:55 +0100

    La til navbar og global css på hjemmesiden

--- Dasi/src/routes/+page.svelte
+++ Dasi/src/routes/+page.svelte
@@ -1,5 +1,10 @@
 <script>
+    import Navbar from '../components/navbar.svelte';
     import CtaButton from '../components/cta-button.svelte';
+    import '/src/styles/global.css';
 </script>
 
+<Navbar/>
 <CtaButton text = "Prøv Oss"/>
+
+<p style="margin-bottom: 200vh;">Halla</p>
