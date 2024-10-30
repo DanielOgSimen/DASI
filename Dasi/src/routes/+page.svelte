434844ba7a8ee11e1e6af54e3a6fb22c08a86564
Daniel Johan Sørby <81819141+DanielJSorby@users.noreply.github.com>
2024-10-30 09:27:55 +0100

    Lgde boxes og la in splash filen

--- Dasi/src/routes/+page.svelte
+++ Dasi/src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script>
     import Navbar from '../components/navbar.svelte';
     import CtaButton from '../components/cta-button.svelte';
+    import Splash from '../boxes/splash.svelte';
     import '/src/styles/global.css';
 </script>
 
 <Navbar/>
+<Splash/>
 <CtaButton text = "Prøv Oss"/>
 
 <p style="margin-bottom: 200vh;">Halla</p>
