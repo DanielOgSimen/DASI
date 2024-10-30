3f97831247328f2748e388891b5e4f487ae790f6
Daniel Johan Sørby <81819141+DanielJSorby@users.noreply.github.com>
2024-10-30 16:44:59 +0100

    Update scrollSection.svelte
    Flytta den en tab lengre inn

--- Dasi/src/boxes/scrollSection.svelte
+++ Dasi/src/boxes/scrollSection.svelte
@@ -14,44 +14,44 @@
 </div>
 
 <style>
-    .input_wrap {
-        margin-top: 20rem;
-        width: auto;
-        height: auto;
-        position: relative;
-    }
+  .input_wrap {
+      margin-top: 20rem;
+      width: auto;
+      height: auto;
+      position: relative;
+  }
 
-    .input_wrap input {
-        width: 100%;
-        padding: 14px;
-        font-size: 16px;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-    }
+  .input_wrap input {
+      width: 100%;
+      padding: 14px;
+      font-size: 16px;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+  }
 
-    .input_wrap label {
-        font-family: arial;
-        font-size: 16px;
-        position: absolute;
-        top: 50%;
-        left: 14px;
-        transform: translateY(-50%);
-        transition: 0.2s ease all;
-        pointer-events: none;
-        background: #ffffff;
-        padding: 0 5px;
-    }
+  .input_wrap label {
+      font-family: arial;
+      font-size: 16px;
+      position: absolute;
+      top: 50%;
+      left: 14px;
+      transform: translateY(-50%);
+      transition: 0.2s ease all;
+      pointer-events: none;
+      background: #ffffff;
+      padding: 0 5px;
+  }
 
-    input:focus + label,
-    input:valid + label {
-        font-size: 12px;
-        top: -5px;
-        left: 10px;
-        background: #ffffff;
-        padding: 0 5px;
-    }
+  input:focus + label,
+  input:valid + label {
+      font-size: 12px;
+      top: -5px;
+      left: 10px;
+      background: #ffffff;
+      padding: 0 5px;
+  }
 
-    input[type="text"]:focus {
-        outline: none;
-    }
+  input[type="text"]:focus {
+      outline: none;
+  }
 </style>
