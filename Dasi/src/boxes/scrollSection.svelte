39bdd8909128d5788a7a7c9b5798f3027737f365
Simen Blien <simeneblien@icloud.com>
2024-11-05 10:57:55 +0100

    lagde component for input

--- Dasi/src/boxes/scrollSection.svelte
+++ Dasi/src/boxes/scrollSection.svelte
@@ -1,24 +1,34 @@
 <script>
-    
+    import Cards from "../components/cards-homepage.svelte";
+    import PromtInput from "../components/input-promt.svelte";
 </script>
 
 <div class="imgOneClick">
     <img style="width: 27rem; object-fit: contain;" src="/src/Images/OneClickPic.png" alt="">  
     <div class="OneClickText">
         <h1 style="color: #E0E0E0; margin-bottom: 1rem;" class="medium">Available in one<br><span class="accent-blue">click</span></h1>
-        <div class="input_wrap">
-            <input type="text" required />
-            <!-- svelte-ignore a11y-label-has-associated-control -->
-            <label>Ask a promt</label>
-            <svg class=input_icon xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 41.621 41.621">
-                <path id="Icon_feather-send" data-name="Icon feather-send" d="M41,3,20.1,23.9M41,3,27.7,41,20.1,23.9,3,16.3Z" transform="translate(-1.5 -0.879)" fill="none" stroke="#00b8d4" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
-              </svg>
-              
-        </div>
+        <PromtInput></PromtInput>
     </div>
 </div>
+<section id="sectionPin">
+    <div class="pin-wrap-sticky">
+        <div class="pin-wrap" style="display: flex; flex-direction: row;">
+            <div class="selection1">
+                <Cards></Cards>
+            </div>
+            <div class="selection2"></div>
+        </div>
+    </div>
+</section>
 
 <style>
+.selection1 {
+    display: flex;
+    flex-direction: row;
+    gap: 5rem;
+    width: 100vw;
+}
+
 .OneClickText {
     display: flex;
     flex-direction: column;
@@ -37,52 +47,43 @@
     height: 100vh;
 }
 
-.input_wrap .input_icon {
-    position: absolute;
-    top: 50%;
-    left: 17rem; /* Position the SVG to the right */
-    transform: translateY(-50%);
-    pointer-events: none; /* Ensure the SVG does not interfere with input interactions */
+
+  @keyframes move {
+	to {
+		/* Move horizontally so that right edge is aligned against the viewport */
+		transform: translateX(calc(-100% + 100vw));
+	}
 }
 
-  .input_wrap {
-      width: auto;
-      height: auto;
-      position: relative;
-  }
+#sectionPin {
+	/* Stretch it out, so that we create room for the horizontal scroll animation */
+	height: 500vh;
+	overflow: visible; /* To make position sticky work … */
 
-  .input_wrap input {
-      width: 20rem;
-      padding: 14px;
-      font-size: 16px;
-      background-color: #242424;
-      border-radius: 10px;
-      border: none;
-  }
+	view-timeline-name: --section-pin-tl;
+	view-timeline-axis: block;
+}
 
-  .input_wrap label {
-      font-family: arial;
-      font-size: 16px;
-      position: absolute;
-      top: 50%;
-      left: 14px;
-      transform: translateY(-50%);
-      transition: 0.2s ease all;
-      pointer-events: none;
-      color: #B0B0B0;
-      padding: 0 5px;
-  }
+.pin-wrap-sticky {
+	/* Stick to Top */
+	height: 100vh;
+	width: 100vw;
+	position: sticky;
+	top: 0;
+	width: 100vw;
+	overflow-x: hidden;
+}
 
-  input:focus + label,
-  input:valid + label {
-      font-size: 12px;
-      top: -5px;
-      left: 10px;
-      color: #E0E0E0;
-      padding: 0 5px;
-  }
+.pin-wrap {
+	height: 100vh;
+	width: 200vmax;
 
-  input[type="text"]:focus {
-      outline: none;
-  }
+	/* Hook animation */
+	will-change: transform;
+	animation: linear move forwards;
+
+	/* Link animation to view-timeline */
+	animation-timeline: --section-pin-tl;
+	animation-range: contain 0% contain 100%;
+}
 </style>
---
+++ Dasi/src/components/input-promt.svelte
@@ -0,0 +1,69 @@
+<script>
+    export let label = "Ask a promt";
+    export let color = "#242424";
+    export let HideSvg = false;
+</script>
+
+<div class="input_wrap">
+    <input style="background-color: {color};" type="text" required />
+    <!-- svelte-ignore a11y-label-has-associated-control -->
+    <label>{label}</label>
+    <svg class:display={HideSvg} class=input_icon xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 41.621 41.621">
+        <path id="Icon_feather-send" data-name="Icon feather-send" d="M41,3,20.1,23.9M41,3,27.7,41,20.1,23.9,3,16.3Z" transform="translate(-1.5 -0.879)" fill="none" stroke="#00b8d4" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"/>
+      </svg>    
+</div>
+
+<style>
+    .display {
+        display: none;
+    }
+
+    .input_wrap .input_icon {
+    position: absolute;
+    top: 50%;
+    left: 17rem; /* Position the SVG to the right */
+    transform: translateY(-50%);
+    pointer-events: none; /* Ensure the SVG does not interfere with input interactions */
+}
+
+  .input_wrap {
+      width: auto;
+      height: auto;
+      position: relative;
+  }
+
+  .input_wrap input {
+      width: 20rem;
+      padding: 14px;
+      font-size: 16px;
+      border-radius: 10px;
+      border: none;
+  }
+
+  .input_wrap label {
+      font-family: arial;
+      font-size: 16px;
+      position: absolute;
+      top: 50%;
+      left: 14px;
+      transform: translateY(-50%);
+      transition: 0.2s ease all;
+      pointer-events: none;
+      color: #B0B0B0;
+      padding: 0 5px;
+  }
+
+  input:focus + label,
+  input:valid + label {
+      font-size: 12px;
+      top: -5px;
+      left: 10px;
+      color: #E0E0E0;
+      padding: 0 5px;
+  }
+
+  input[type="text"]:focus {
+      outline: none;
+  }
+
+</style>
