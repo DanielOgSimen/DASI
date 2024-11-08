6e6da3779df2450fc0dd3d8c1a3f4f5fa8992297
Simen Blien <simeneblien@icloud.com>
2024-11-08 13:32:36 +0100

    subscribtion and scroll animation done

--- Dasi/src/components/boxes/subscribtionCard.svelte
+++ Dasi/src/components/boxes/subscribtionCard.svelte
@@ -1,18 +1,42 @@
 <div class="subCard">
-    <h1>Starter</h1>
-    <p>For beginners and normal people</p>
+    <h1 class="bigText bold">Starter</h1>
+    <p style="font-size: 15px; margin-bottom:0.5rem;">For beginners and normal people</p>
     <div class="price">
-        <h1>$10<span style="font-size: 10px;">.00</span></h1>
-        <p></p>
+        <h1 class="bigText bold">$10<span style="font-size: 17px;">.00</span></h1>
+        <p style="font-size:14px;">USD/mo(annually)</p>
     </div>
     <button class="button purple">Go to Payment</button>
+    <ul style="font-size: 16px;">
+        <li>Access to all content</li>
+        <li>Access to all content</li>
+        <li>Access to all content</li>
+        <li>Access to all content</li>
+    </ul>
 </div>
 
 <style>
+    .bigText {
+        font-size: 40px;
+    }
+
+    ul {
+        display: flex;
+        margin-top: 1rem;
+        flex-direction: column;
+        list-style-position: inside;
+    }
+
+    .price {
+        display: flex;
+        flex-direction: row;
+        align-items: baseline;
+        gap: 0.7rem;
+        margin-bottom: 0.5rem;
+    }
     .subCard {
         background-color: #E0E0E0;
         color: #242424;
-        padding: 30px 20px 200px 20px;
-        border-radius: 60px;
+        padding: 80px 80px 120px 80px; 
+        border-radius: 40px;
     }
 </style>
