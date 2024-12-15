cc41d7499309baa8c4818840e1c4eebfb66fcad3
Simen Blien <simeneblien@icloud.com>
2024-12-15 12:25:16 +0100

    variabler på sub card

--- Dasi/src/components/boxes/subscribtionCard.svelte
+++ Dasi/src/components/boxes/subscribtionCard.svelte
@@ -1,20 +1,35 @@
+<script>
+    export let SubscriptionType = "Starter";
+    export let SubscriptionPrice = "$10";
+    export let buttonText = "Go to Payment";
+    export let SubscriptionIntro = "For beginners and normal people";
+    export let SubscriptionContent = `
+    <li>Access to all content</li>
+    <li>Access to all features</li>
+    <li>Access to all updates</li>
+    <li>Basic chatbot functionality</li>
+    <li>Limited customization and integrations</li>
+    <li>Suitable for small websites or simple customer service tasks.</li>
+    <li>Limited number of messages or interactions per month.</li>`;
+</script>
+
 <div class="subCard">
-    <h1 class="bigText bold">Starter</h1>
-    <p style="font-size: 15px; margin-bottom:0.5rem;">For beginners and normal people</p>
+    <h1 class="bigText bold">{SubscriptionType}</h1>
+    <p style="font-size: 15px; margin-bottom:0.5rem;">{SubscriptionIntro}</p>
     <div class="price">
-        <h1 class="bigText bold">$10<span style="font-size: 17px;">.00</span></h1>
+        <h1 class="bigText bold">{SubscriptionPrice}<span style="font-size: 17px;">.00</span></h1>
         <p style="font-size:14px;">USD/mo(annually)</p>
     </div>
-    <button class="button purple">Go to Payment</button>
-    <ul style="font-size: 16px;">
-        <li>Access to all content</li>
-        <li>Access to all content</li>
-        <li>Access to all content</li>
-        <li>Access to all content</li>
+    <button class="button purple">{buttonText}</button>
+    <ul class="subCardInfo">
+        {@html SubscriptionContent}
     </ul>
 </div>
 
 <style>
+    .subCardInfo {
+        font-size: 16px;
+    }
     .bigText {
         font-size: 40px;
     }
@@ -38,10 +53,16 @@
         color: #242424;
         padding: 80px 80px 120px 80px; 
         border-radius: 40px;
+        margin-left: 2rem;
     }
     @media (max-width: 700px) {
         .subCard {
             padding: 40px 40px 60px 40px; 
+        }
+    }
+    @media (max-width: 940px) {
+        .subCard {
+            margin-left: 0;
         }
     }
 </style>
