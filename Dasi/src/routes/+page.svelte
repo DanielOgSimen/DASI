0cdab072a403ac45a23d1791674679ed7d2e2290
Daniel Johan Sørby <81819141+DanielJSorby@users.noreply.github.com>
2025-01-18 09:52:23 +0100

    fjerna ubrukte imports

--- Dasi/src/routes/+page.svelte
+++ Dasi/src/routes/+page.svelte
@@ -1,6 +1,4 @@
 <script>
-    import Navbar from '../components/navbar.svelte';
-    import CtaButton from '../components/cta-button.svelte';
     import Splash from '../boxes/splash.svelte';
     import ScrollSelection from '../boxes/scrollSection.svelte';
 </script>
