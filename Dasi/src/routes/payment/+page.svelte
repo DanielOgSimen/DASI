b9b6453e61aa4aba727b7d3eec8c4ea747386318
Simen Blien <simeneblien@icloud.com>
2025-03-07 15:01:01 +0100

    oppdatrerte js

--- Dasi/src/routes/payment/+page.svelte
+++ Dasi/src/routes/payment/+page.svelte
@@ -43,7 +43,7 @@
         if (error) {
             paymentMessage = error.message;
         } else {
-            const response = await fetch("/api/create-payment-intent", {
+            const response = await fetch("/api/create-payment-intent.js", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ paymentMethodId: paymentMethod.id })
