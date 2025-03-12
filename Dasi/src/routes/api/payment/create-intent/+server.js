da9ff3b12572563fbd6457606a0412b47a931c35
Simen Blien <simeneblien@icloud.com>
2025-03-12 09:32:05 +0100

    kommenterte ut server.js

--- Dasi/src/routes/api/payment/create-intent/+server.js
+++ Dasi/src/routes/api/payment/create-intent/+server.js
@@ -1,4 +1,4 @@
-import express from 'express';
+/* import express from 'express';
 import Stripe from 'stripe';
 
 const app = express();
@@ -32,4 +32,4 @@ app.post('/api/payment/create-intent', async (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});
+}); */
