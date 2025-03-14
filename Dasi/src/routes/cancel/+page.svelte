aa8d9ab3828786e5b5c7a024e70ad835e3a62827
Simen Blien <simeneblien@icloud.com>
2025-03-14 18:33:15 +0100

    lites opdatering på suksess sidene

--- Dasi/src/routes/cancel/+page.svelte
+++ Dasi/src/routes/cancel/+page.svelte
@@ -1,1 +1,6 @@
 <h1>The payment was canceled</h1>
+
+<style>
+    h1 {
+        margin-top: 120px;
+    }
--- Dasi/src/routes/success/+page.svelte
+++ Dasi/src/routes/success/+page.svelte
@@ -1,1 +1,7 @@
 <h1>Order receved</h1>
+
+<style>
+    h1 {
+        margin-top: 120px;
+    }
+</style>
