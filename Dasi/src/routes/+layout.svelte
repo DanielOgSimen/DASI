1edc111f4a1e59ee83eed543072fd8558fae1909
Daniel Johan Sørby <81819141+DanielJSorby@users.noreply.github.com>
2025-03-18 14:28:34 +0100

    Update +layout.svelte

--- Dasi/src/routes/+layout.svelte
+++ Dasi/src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
-    
+    import { initializeApp } from "firebase/app";
+    import { getAnalytics, isSupported } from "firebase/analytics";
+    import { getDatabase, ref, set } from "firebase/database";
     import Navbar from "../components/navbar.svelte";
     import "/src/styles/global.css";
     import ToTopButton from "../components/toTopButton.svelte";
     import Coockies from "../components/coockies/coockies.svelte";
+
     interface Props {
         children?: import('svelte').Snippet;
     }
